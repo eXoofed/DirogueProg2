96b5ec7acd7c3d144a3329938821452117356119
André Garcia <andre.garcia.protic@gmail.com>
2024-10-23 18:15:36 -0400

    Structure de MonLabyrinthe.java

--- src/labyrinthe/soumission/MonLabyrinthe.java
+++ src/labyrinthe/soumission/MonLabyrinthe.java
@@ -1,5 +1,38 @@
 package labyrinthe.soumission;
+import labyrinthe.code_squelette.Exterieur;
+import labyrinthe.code_squelette.Labyrinthe;
+import labyrinthe.code_squelette.Piece;
 
-public class MonLabyrinthe { //TODO: extends ? implements? 
+public class MonLabyrinthe implements Labyrinthe { //TODO: extends ? implements?
 
+
+    @Override
+    public Piece[] getPieces() {
+        return new Piece[0];
+    }
+
+    @Override
+    public int nombreDePieces() {
+        return 0;
+    }
+
+    @Override
+    public void ajouteEntree(Exterieur out, Piece e) {
+
+    }
+
+    @Override
+    public void ajouteCorridor(Piece e1, Piece e2) {
+
+    }
+
+    @Override
+    public boolean existeCorridorEntre(Piece e1, Piece e2) {
+        return false;
+    }
+
+    @Override
+    public Piece[] getPiecesConnectees(Piece e) {
+        return new Piece[0];
+    }
 }
