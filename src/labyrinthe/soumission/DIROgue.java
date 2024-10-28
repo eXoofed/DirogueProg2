6e3863d63d102f73b5e5676590d5c998c763162f
William Perktold <william.perktold@umontreal.ca>
2024-10-28 17:09:14 -0400

    complété genererScenario()

--- src/labyrinthe/soumission/DIROgue.java
+++ src/labyrinthe/soumission/DIROgue.java
@@ -1,6 +1,9 @@
 package labyrinthe.soumission;
 
 import labyrinthe.code_squelette.*;
+import labyrinthe.rencontres.*;
+
+import java.util.Random;
 
 public class DIROgue {
 
@@ -29,7 +32,42 @@ public class DIROgue {
 	}
 
 	public static String genererScenario(Aventure a) {
-		// TODO: à remplir!
+		Piece[] chemin = a.cheminJusquAuBoss();
+		String scenario = "";
+		for (int i = 0; i < chemin.length; i++) {
+			Rencontre x = null;
+			if (chemin[i].getRencontreType() == RencontreType.BOSS)
+				x = new Boss();
+			else if (chemin[i].getRencontreType() == RencontreType.RIEN)
+				x = new Rien();
+			else if (chemin[i].getRencontreType() == RencontreType.TRESOR) {
+				int r = new Random().nextInt(3);
+				switch (r) {
+					case 0:
+						x = new SacDeButin(); //… choix 1
+						break;
+					case 1:
+						x = new Potion(); //… choix 2
+						break;
+					case 2:
+						x = new ArtefactMagique(); //… choix 3
+				}
+			}
+			else if (chemin[i].getRencontreType() == RencontreType.MONSTRE) {
+				int r = new Random().nextInt(3);
+				switch (r) {
+					case 0:
+						x = new Gobelin(); //… choix 1
+						break;
+					case 1:
+						x = new Orque(); //… choix 2
+						break;
+					case 2:
+						x = new Gargouille(); //… choix 3
+				}
+			}
+			scenario += x.rencontrer() + "/n";
+		}
 		return null;
 	}
 
